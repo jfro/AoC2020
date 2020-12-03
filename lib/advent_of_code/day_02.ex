96a8286ad88ae96d0522e89eea1a9a0029ef68c1
Jeremy Knope <jeremy@astropad.com>
2020-12-02 21:32:48 -0500

    d02p2 solution & tests

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -2,13 +2,27 @@ defmodule AdventOfCode.Day02 do
   def check_pass(pass_line) do
     [_match, min, max, letter, string] = Regex.run(~r/(\d+)\-(\d+)\s(\w):\s+(\w+)/, pass_line)
     map = String.codepoints(string) |> Enum.frequencies()
-    # IO.puts("#{letter}: #{map[letter]} within #{min} #{max}")
     min = String.to_integer(min)
     max = String.to_integer(max)
 
     case map[letter] do
       nil -> false
       c -> c >= min && c <= max
+    end
+  end
+
+  def check_pass2(pass_line) do
+    [_match, pos1, pos2, letter, string] = Regex.run(~r/(\d+)\-(\d+)\s(\w):\s+(\w+)/, pass_line)
+    chars = String.codepoints(string)
+    pos1 = String.to_integer(pos1) - 1
+    pos2 = String.to_integer(pos2) - 1
+    char1 = chars |> Enum.at(pos1)
+    char2 = chars |> Enum.at(pos2)
+
+    cond do
+      char1 == char2 -> false
+      char1 == letter || char2 == letter -> true
+      true -> false
     end
   end
 
@@ -21,5 +35,10 @@ defmodule AdventOfCode.Day02 do
   end
 
   def part2(args) do
+    args
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&check_pass2/1)
+    |> Stream.filter(fn x -> x end)
+    |> Enum.count()
   end
 end
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -3,19 +3,17 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = File.stream!("data/day02p1.txt")
     result = part1(input)
 
-    assert result
+    assert result == 546
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = File.stream!("data/day02p1.txt")
     result = part2(input)
 
-    assert result
+    assert result == 275
   end
 end
