0df9c0cbf1abcd3c5f1896d30c52bf58a27b1edc
Jeremy Knope <jeremy@astro-hq.com>
2020-12-04 13:26:33 -0500

    stubs for day 3 & 4. Adds solve mix task to run solutions

--- lib/mix/tasks/solve.ex
+++ lib/mix/tasks/solve.ex
@@ -2,6 +2,33 @@ defmodule Mix.Tasks.Solve do
   use Mix.Task
 
   def run(args) do
-    IO.puts("TODO...")
+    {year, day, bench, part} = args |> parse_args()
+    part_fn = String.to_atom("p#{part}")
+
+    run = fn ->
+      apply(String.to_existing_atom("Elixir.Y#{year}.D#{day}"), part_fn, [])
+    end
+
+    # result = IO.puts("Result: #{result}")
+
+    if bench,
+      do: Benchee.run(%{part_1: run}),
+      else:
+        run.()
+        |> IO.inspect(label: "#{year} Day #{day} Part #{part} Results")
+  end
+
+  defp parse_args(args) do
+    {kw, _, _} =
+      OptionParser.parse(
+        args,
+        aliases: [y: :year, d: :day, b: :bench, p: :part],
+        strict: [year: :integer, day: :integer, bench: :boolean, part: :integer]
+      )
+
+    year = AOC.Helpers.year()
+    day = AOC.Helpers.day()
+
+    {kw[:year] || year, kw[:day] || day, kw[:bench] || false, kw[:part] || 1}
   end
 end
